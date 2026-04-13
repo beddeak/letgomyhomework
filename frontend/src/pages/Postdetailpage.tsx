5986fdf7144de823f0dce074654ea7650b3dd0b5
beddeak <bedding1083@gmail.com>
2026-04-14 01:48:13 +0900

    포스트 디테일, 타이틀 상세페이지와 편집페이지 기본 구조

--- frontend/src/pages/Postdetailpage.tsx
+++ frontend/src/pages/Postdetailpage.tsx
@@ -1,3 +1,39 @@
-export default function Postdetailpage() {
-    return <div>포스트 디테일 페이지</div>
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+ 
+
+interface Post {
+    title:string;
+    id:number;
+    content:string;
 }
+
+
+const posts: Post[] = [
+    {title:"처음왔어요",id:1,content:"신규유저"},
+    {title:"나는누구",id:2,content:"나는누구"},
+    {title:"엄준식",id:3,content:"흠"}
+];
+
+
+function PostDetailPage() {
+    const {id} = useParams();
+    const postId = Number(id);
+    const post = posts.find(post => post.id === postId);
+    
+    if (!post) {
+        return <p>글을 찾을수가 읎어요</p>
+    }
+
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            <Link to={`/posts/${post.id}/edit`}>수정하러가기</Link>
+        </div>
+    )
+}
+
+
+
+export default PostDetailPage
--- frontend/src/pages/Posteditpage.tsx
+++ frontend/src/pages/Posteditpage.tsx
@@ -1,3 +1,39 @@
-export default function Posteditpage() {
-    return <div>페이지 수정 페이지</div>
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+  
+
+interface Post {
+    title:string;
+    id:number;
+    content:string;
 }
+
+
+const posts: Post[] = [
+    {title:"처음왔어요",id:1,content:"신규유저"},
+    {title:"나는누구",id:2,content:"나는누구"},
+    {title:"엄준식",id:3,content:"흠"}
+]
+
+function PostEditPage() {
+    const {id} = useParams();
+    const PostId = Number(id);
+    const post = posts.find(post => post.id === PostId);
+    const [title,setTitle] = useState(post?.title ?? "")
+    const [content,setContent] = useState(post?.content ?? "")
+
+    if (!post) {
+        return <p>글을 찾을수가 없습니다</p>
+    }
+
+    return (
+        <div>
+            <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <textarea value={content} onChange={(e) => setContent(e.target.value)}>입력하세요</textarea>
+        </div>
+    )
+}
+
+
+
+export default PostEditPage
